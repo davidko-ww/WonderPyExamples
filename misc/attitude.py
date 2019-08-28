0861d665aacc504d0a11bd66213d1550c27b026c
David Ko <david.ko@makewonder.com>
2019-08-28 13:30:38 -0700

    Added roll/pitch/slope "types" to attitude example

--- misc/attitude.py
+++ misc/attitude.py
@@ -5,7 +5,13 @@ import WonderPy.core.wwMain
 class MyClass(object):
 
     def on_sensors(self, robot):
-        print('({}, {})'.format(robot.sensors.attitude.roll, robot.sensors.attitude.pitch))
+        print('({}, {}, {}) ({}, {}, {})'.format(
+            robot.sensors.attitude.roll, 
+            robot.sensors.attitude.pitch,
+            robot.sensors.attitude.slope,
+            robot.sensors.attitude.roll_type,
+            robot.sensors.attitude.pitch_type,
+            robot.sensors.attitude.slope_type))
 
 if __name__ == "__main__":
     WonderPy.core.wwMain.start(MyClass())
